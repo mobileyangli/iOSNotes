4e0c8a98dfd2356a7ab573fef575534820f26cf2
mobileyangli <mobile_yangli@163.com>
2016-03-24 10:42:32 +0800

    加入bound注释说明

--- Folders/1 View Controllers/NeatTableViewController/ISNNeatViewController.m
+++ Folders/1 View Controllers/NeatTableViewController/ISNNeatViewController.m
@@ -27,6 +27,7 @@
     // tableview controller 中可以使用parentcontroller来访问它的持有者
     [self addChildViewController:self.tableViewController];
 
+    // 布局需要坐标信息，获取参照view的bounds要比frame准确，因为frame是相对父坐标的位置rect
     CGRect frame = CGRectInset(self.view.bounds, 0, self.bannerImageView.frame.size.height / 2);
     frame.origin.y = self.bannerImageView.frame.size.height;
     self.tableViewController.tableView.frame = frame;

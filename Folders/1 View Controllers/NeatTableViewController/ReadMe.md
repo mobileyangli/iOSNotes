80d5b4c07709f4ad37d7afad090b1f92f89f0c2d
mobileyangli <mobile_yangli@163.com>
2016-04-15 11:43:40 +0800

    加入MD文件测试

---
+++ Folders/1 View Controllers/NeatTableViewController/ReadMe.md
@@ -0,0 +1,8 @@
+
+## 设计期望 ##
+
+----------
+
+1. 通过嵌入方式加入tableview controller，可以将tableview的实现和业务controller实现分离，业务controller只关注业务逻辑，tableview controller关注tableview的实现
+
+2. tableview controller可以复用在其他地方，可以单独测试
---
+++ Folders/2 Views/CollectionView+Dynamics/ISNCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  ISNCollectionViewController.h
+//  iOSNotes
+//
+//  Created by 杨冬凌 on 16/4/12.
+//  Copyright © 2016年 yangdongling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISNCollectionViewController : UICollectionViewController
+
+@end
